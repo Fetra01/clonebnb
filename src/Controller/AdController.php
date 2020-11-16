c02c4fe2b75f4081f06705372a14c5ca408b4d2e
Fetra01 <fetra.andria@outlook.fr>
2020-11-16 13:47:45 +0100

    Création du formulaire d'annonce, soumitionet mise en form du template

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +28,46 @@ class AdController extends AbstractController
             'ads' => $ads
         ]);
     }
+
+
+
+    /**
+     * Permet de créer une annonce
+     * 
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response
+     */
+    public function create(Request $request,EntityManagerInterface $manager){
+        $ad = new Ad();
+        
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+            
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+       
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
 
 
     /**
