62ca20da4eed641a5310b92a51c6ae97bcba9c22
Fetra01 <fetra.andria@outlook.fr>
2020-11-19 11:57:28 +0100

    Gestion des annonces, formulaire de création et édition

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -3,14 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class AdType extends AbstractType
 {
@@ -20,27 +22,37 @@ class AdType extends AbstractType
      *
      * @param string $label
      * @param string $placeholder
+     * @param array $options
      * @return array
      */
-    private function getConfiguration($label, $placeholder){
-        return [
+    private function getConfiguration($label, $placeholder, $options = []){
+        return array_merge([
             'label' => $label,
             'attr' => [
                 'placeholder' => $placeholder
-            ]
-        ];
+            ],
+        ], $options);
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title', TextType::class, $this->getConfiguration("Titre", "Tapez un super titre pour votre annonce !"))
-            //->add('slug', TextType::class,$this->getConfiguration("Chaine URL", "Adresse web (automatique)"))
+            ->add('slug', TextType::class,$this->getConfiguration("Chaine URL", "Adresse web (automatique)",[
+                'required' => false
+            ]))
             ->add('coverImage', UrlType::class, $this->getConfiguration("URL de l'image principale", "Donnez l'adresse d'une image qui donne vraiment envie de venir chez vous !"))
             ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Donnez une déscription globale de l'annonce"))
             ->add('Content', TextareaType::class, $this->getConfiguration("Description détaillée", "Tapez une description qui donne vraiment envie de venir chez vous !"))
             ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", "Le nombre de chambre disponible !"))
             ->add('price', MoneyType::class, $this->getConfiguration("Prix par nuit", "Indiquez le prix que vous voulez pour une nuit"))
+            ->add('images', CollectionType::class,
+                [
+                    'entry_type'    => ImageType::class,
+                    'allow_add'     => true,
+                    'allow_delete'  => true
+                ]
+            )
         ;
     }
 
