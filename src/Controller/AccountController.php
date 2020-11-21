95d8c3833f629e6b709965a6b5d0dc139945420d
Fetra01 <fetra.andria@outlook.fr>
2020-11-21 13:59:37 +0100

    Modification profile et de mot de passe terminer

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\PasswordUpdate;
 use App\Entity\User;
+use App\Form\AccountType;
+use App\Form\PasswordUpdateType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -80,6 +85,90 @@ class AccountController extends AbstractController
         }
 
         return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    
+    /**
+     * Permet d'afficher et de traiter le formulaire de modification de profil
+     * 
+     * @Route("/account/profile", name="account_profile")
+     * 
+     *
+     * @return Response
+     */
+    public function profile(Request $request, EntityManagerInterface $manager)
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($user);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                "Les données du profil ont été enregistrée avec succè !"
+            );
+
+            return $this->redirectToRoute('ads_index');
+
+        }
+
+        return $this->render('account/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de modifier le mot de passe 
+     * 
+     * @Route("/account/password-update", name="account_password")
+     * 
+     * @return Response
+     * 
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, EntityManagerInterface $manager)
+    {
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //1.Vérifier que le oldPassword du formulaire soit le même que le passord de l'user
+
+            if(password_verify($passwordUpdate->getOldPassword(),$user->getHash())){
+                //Gérer l'erreur
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez tapé n'est pas le mot de passe actuel !"));
+            }
+            else{
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setHash($hash);
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifier !"
+                );
+
+                return $this->redirectToRoute('homepage');
+
+            }
+        }
+
+        
+        return $this->render('account/password.html.twig', [
             'form' => $form->createView()
         ]);
     }
